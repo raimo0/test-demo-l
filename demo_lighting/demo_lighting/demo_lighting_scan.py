20784dc8be9c179296c223b43ad2f92c25390b0b
Raimo Köidam <raimokoidam@gmail.com>
2024-02-26 13:01:58 +0200

    a

--- demo_lighting/demo_lighting/demo_lighting_scan.py
+++ demo_lighting/demo_lighting/demo_lighting_scan.py
@@ -9,13 +9,22 @@ pub_mode = None
 def update_ranges(ranges):
     led_msg_mode = LedModuleMode()
     color = ColorRGB()
-    color.r = min(int(ranges.left*100), 255)
-    color.g = min(int(ranges.right*100), 255)
-    color.b = min(int(ranges.front*100), 255)
-    led_msg_mode.mode = led_msg_mode.SPIN
+    try:
+        color.r = min(int(ranges.left*100), 255)
+    except:
+        color.r = 255
+    try:
+        color.g = min(int(ranges.right*100), 255)
+    except:
+        color.g = 255
+    try:
+        color.b = min(int(ranges.front*100), 255)
+    except:
+        color.b = 255
+    led_msg_mode.mode = 7
     led_msg_mode.color = color
     pub_mode.publish(led_msg_mode)
-    #node.get_logger().info('Publishing: "%s"' % led_msg_mode)
+    node.get_logger().info('Publishing: "%s"' % led_msg_mode)
 
 def main(args=None):
     global pub_mode, node
